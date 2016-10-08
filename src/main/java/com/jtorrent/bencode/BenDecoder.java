d6c933430ede5f26d4060650364628554bef95c5
Philip Geske <pgeske@Philips-MacBook-Pro.local>
2016-10-08 13:14:09 -0400

    Outlined decoder

--- src/main/java/com/jtorrent/bencode/BenDecoder.java
+++ src/main/java/com/jtorrent/bencode/BenDecoder.java
@@ -10,20 +10,25 @@ public class BenDecoder {
 
     public static BenItem decode(String bencode) {
         BenItem cur;
-        // 
-//        String curType = null;
-//        if (bencode.charAt(0) == 'l') {
-//            cur = new BenItem(new ArrayList<BenItem>());
-//        }
-//        else if (bencode.charAt(0) == 'd') {
-//            cur = new BenItem(new HashMap<String, BenItem>());
-//        }
-//        for (int i = 1; i < bencode.length(); i++) {
-//            //
-//            if (bencode.charAt(i) == 'i' && curType == null) {
-//                curType = 'i';
-//            }
-//            elsif (bencode.charAt(i) == '')
-//        }
+        BenItem result;
+        int index;
+        // Get initial BenItem
+            //TODO
+
+        while (cur != null) {
+            // Starting new item: create new BenItem and set parent to cur
+                //TODO
+
+            // In the middle of item: add chars to accumulator
+                //TODO
+
+            // Finished item: store accumulated value in cur, push cur to parent, and set cur to parent
+                //TODO
+                // If cur's parent is null, then we must be finished decoding
+        }
+
+        return result;
+
+
     }
 }
