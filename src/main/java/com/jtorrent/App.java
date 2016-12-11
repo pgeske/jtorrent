5234b9072f3f0a44661e07c22ee95fb3ddd5218a
pgeske <pwg46@cornell.edu>
2016-12-10 20:01:36 -0500

    Parsing compressed peer binary data.

--- src/main/java/com/jtorrent/App.java
+++ src/main/java/com/jtorrent/App.java
@@ -23,8 +23,12 @@ public class App
         Tracker tracker = new Tracker(bi.find("announce"), bi.find("info"), "akdlakdiejsldifjelsk");
         URL request = tracker.getRequest();
         BenItem trackerResponse = tracker.getResponse();
-        String compactPeers = trackerResponse.find("peers").getString();
-        System.out.println(compactPeers);
+        byte[] compactPeers = trackerResponse.find("peers").getString().getBytes("ISO-8859-1");
+        for (int i = 0; i < 4; i++) {
+            int x = (compactPeers[i] & 0xFF);
+            System.out.println(x);
+        }
+//        System.out.println(compactPeers);
 //        System.out.println(peers);
 //        for (String key : peers.keySet()) {
 //            System.out.println(key + ": " + peers.get(key));
